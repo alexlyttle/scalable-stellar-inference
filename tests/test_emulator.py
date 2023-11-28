9a68644522d0eec48e4a332461c7a1efe357e08a
Alex Lyttle <a.j.lyttle@outlook.com>
2023-11-28 13:48:10 +0000

    Add test content

--- tests/test_emulator.py
+++ tests/test_emulator.py
@@ -1,13 +1,31 @@
+import pytest
+import numpy as np
 from celestify.emulator import Emulator
 
-def test_emulator_numpy():
-    pass
+backends = ["numpy", "jax", "pytensor", "tensorflow"]
+alternate_backends = ["np", "pt", "tf"]
+inputs = 5 * [0.0]
 
-def test_emulator_jax():
-    pass
+@pytest.mark.parametrize("backend", backends+alternate_backends)
+def test_emulator_init(backend):
+    """Test emulator init method works."""
+    _ = Emulator(backend=backend)
 
-def test_emulator_pytensor():
-    pass
+@pytest.mark.parametrize("backend", backends)
+def test_emulator_call(backend):
+    """Test emulator call method works."""
+    emulator = Emulator(backend=backend)
+    _ = emulator(inputs)
 
-def test_emulator_tensorflow():
-    pass
+def test_emulator_consistency():
+    """Test the consistency of the emulator output between backends."""
+    first_output = None
+    for backend in backends:
+        emulator = Emulator(backend=backend)
+        output = emulator(inputs)
+        if backend == "pytensor":
+            output = output.eval()
+        if first_output is None:
+            first_output = output
+        else:
+            assert np.allclose(output, first_output)
