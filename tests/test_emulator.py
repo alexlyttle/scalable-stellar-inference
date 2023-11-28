b18f7d1b9739e84da87ebd39ffe619ee2ceee015
Alex Lyttle <a.j.lyttle@outlook.com>
2023-11-28 11:28:24 +0000

    Add tests

---
+++ tests/test_emulator.py
@@ -0,0 +1,13 @@
+from celestify.emulator import Emulator
+
+def test_emulator_numpy():
+    pass
+
+def test_emulator_jax():
+    pass
+
+def test_emulator_pytensor():
+    pass
+
+def test_emulator_tensorflow():
+    pass
