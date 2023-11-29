2586b1f563ca0539a2a40b54fdc4b6fa57445c18
Alex Lyttle <a.j.lyttle@outlook.com>
2023-11-29 11:39:57 +0000

    Add bolometric corrections to numpyro model

--- src/celestify/numpyro_models.py
+++ src/celestify/numpyro_models.py
@@ -10,7 +10,7 @@ class SingleStarModel:
     def __init__(self, const=None, bands=None):
         self.star = Star(bands=bands, backend="jax")
         self.const = self._default_const(const=const)
-        self.bands = bands
+        self.photometry = False if bands is None else True
 
     def _default_const(self, const=None):
         if const is None:
@@ -19,6 +19,8 @@ class SingleStarModel:
         const.setdefault("evol", dict(concentration1=2.0, concentration0=5.0))
         const.setdefault("log_mass", dict(loc=0.0, scale=0.3))
         const.setdefault("M_H", dict(loc=0.0, scale=0.5))
+        const.setdefault("distance", dict(concentration=3.0, rate=1e-3))
+        const.setdefault("Av", dict(loc=1.0, scale=1.0))  # TODO: correlate with distance?
         return const
 
     def star_prior(self):
@@ -35,13 +37,19 @@ class SingleStarModel:
         params["Y"] = numpyro.sample("Y", dist.Uniform(low=0.22, high=0.32))
         params["a_MLT"] = numpyro.sample("a_MLT", dist.Uniform(low=1.3, high=2.7))
         return params
+    
+    def bc_prior(self):
+        params = {}
+        params["distance"] = numpyro.sample("distance", dist.Gamma(**self.const["distance"]))
+        params["Av"] = numpyro.sample("Av", dist.TruncatedNormal(**self.const["Av"], low=0.0, high=6.0))
+        return params
 
     def __call__(self, obs=None):
         params = self.star_prior()
 
         # TODO: if self.star.bands is not None, need prior on parallax and extinction
-        if self.star.bands is not None:
-            pass
+        if self.photometry:
+            params.update(self.bc_prior())
 
         determs = self.star(params)
         
--- tests/test_numpyro_models.py
+++ tests/test_numpyro_models.py
@@ -1,14 +1,17 @@
-from pprint import pprint
+import pytest
 from jax import random
 from numpyro import handlers
 from celestify.numpyro_models import SingleStarModel
 
-def test_single_star_init():
-    _ = SingleStarModel()
+bands = [["BP", "RP", "G"], ["H", "J", "K"]]
 
-def test_single_star_trace():
-    model = SingleStarModel()
+@pytest.mark.parametrize("bands", bands)
+def test_single_star_init(bands):
+    _ = SingleStarModel(bands=bands)
+
+@pytest.mark.parametrize("bands", bands)
+def test_single_star_trace(bands):
+    model = SingleStarModel(bands=bands)
 
     rng_seed = random.PRNGKey(0)
-    trace = handlers.trace(handlers.seed(model, rng_seed=rng_seed)).get_trace(obs=None)
-    pprint(trace)
+    _ = handlers.trace(handlers.seed(model, rng_seed=rng_seed)).get_trace(obs=None)
