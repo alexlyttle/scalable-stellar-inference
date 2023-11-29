bd2d20e167932246bf529ac2a4581bdfcea3cb12
Alex Lyttle <a.j.lyttle@outlook.com>
2023-11-29 14:15:29 +0000

    Add star backends

---
+++ src/celestify/_jax/star.py
@@ -0,0 +1,45 @@
+import jax.numpy as jnp
+from ..star import StarBase
+
+
+class Star(StarBase):
+
+    def model(self, params: dict) -> dict:
+        """Predict from model params."""
+        # TODO: break up into a few functions
+        determs = {}  # deterministic parameters to return
+        log_mass = params["log_mass"]
+        mh = params["M_H"]
+
+        determs["mass"] = mass = 10**log_mass
+        inputs = jnp.stack(
+            [params["evol"], mass, mh, params["Y"], params["a_MLT"]], 
+            axis=-1
+        )
+        outputs = self.emulator(inputs).squeeze()
+
+        determs["log_age"] = log_age = outputs[0] - 9  # log(age) in Gyr
+        determs["log_Teff"] = log_teff = outputs[1]
+        determs["log_R"] = log_rad = outputs[2]
+        determs["log_Dnu"] = outputs[3]
+        determs["log_g"] = log_g = log_mass - 2 * log_rad + self.log_g_sun
+        determs["log_L"] = log_lum = 2 * log_rad + 4 * (log_teff - self.log_teff_sun)
+
+        determs["Teff"] = teff = 10**log_teff
+        determs["R"] = 10**log_rad
+        determs["age"] = 10**log_age
+
+        if self.bolometric_corrections is None:
+            return determs
+
+        inputs = jnp.stack(
+            [teff, log_g, mh, params["Av"]],
+            axis=-1
+        )
+        bc = self.bolometric_corrections(inputs).squeeze()
+        determs["bol_mag"] = bol_mag = self.bol_mag_sun - 2.5 * log_lum
+        determs["abs_mag"] = abs_mag = bol_mag - bc
+        determs["mag"] = abs_mag - 5 * (1 - jnp.log10(params["distance"]))
+        determs["plx"] = 1 / params["distance"]
+
+        return determs
--- src/celestify/star.py
+++ src/celestify/star.py
@@ -5,7 +5,8 @@ from math import log10
 
 grav_constant = 6.6743e-8  # in cgs units
 
-class Star:
+
+class StarBase:
     log_m_sun = log10(1.988410) + 33
     log_r_sun = log10(6.957) + 10
     log_g_sun = log10(grav_constant) + log_m_sun - 2 * log_r_sun
@@ -20,55 +21,17 @@ class Star:
             bolometric_corrections = BolometricCorrections(bands, backend=backend)
         self.bolometric_corrections = bolometric_corrections
 
-        if backend == "jax":
-            self.model = self._model_jax()
-        else:
-            message = f"Backend value {backend!r} is invalid."
-            raise ValueError(message)
-
-    def _model_jax(self) -> Callable:
-        import jax.numpy as jnp
-
-        def model(params: dict) -> dict:
-            """Predict from model params."""
-            determs = {}  # deterministic parameters to return
-            log_mass = params["log_mass"]
-            mh = params["M_H"]
-
-            determs["mass"] = mass = 10**log_mass
-            inputs = jnp.stack(
-                [params["evol"], mass, mh, params["Y"], params["a_MLT"]], 
-                axis=-1
-            )
-            outputs = self.emulator(inputs).squeeze()
-
-            determs["log_age"] = log_age = outputs[0] - 9  # log(age) in Gyr
-            determs["log_Teff"] = log_teff = outputs[1]
-            determs["log_R"] = log_rad = outputs[2]
-            determs["log_Dnu"] = outputs[3]
-            determs["log_g"] = log_g = log_mass - 2 * log_rad + self.log_g_sun
-            determs["log_L"] = log_lum = 2 * log_rad + 4 * (log_teff - self.log_teff_sun)
-
-            determs["Teff"] = teff = 10**log_teff
-            determs["R"] = 10**log_rad
-            determs["age"] = 10**log_age
-
-            # TODO: if self.bands is not None, calculate magnitudes and assume parallax and extinciton
-            if self.bolometric_corrections is None:
-                return determs
-
-            inputs = jnp.stack(
-                [teff, log_g, mh, params["Av"]],
-                axis=-1
-            )
-            bc = self.bolometric_corrections(inputs).squeeze()
-            determs["bol_mag"] = bol_mag = self.bol_mag_sun - 2.5 * log_lum
-            determs["abs_mag"] = abs_mag = bol_mag - bc
-            determs["mag"] = abs_mag - 5 * (1 - jnp.log10(params["distance"]))
-            determs["plx"] = 1 / params["distance"]
-
-            return determs
-        return model
+    def model(self, params: dict) -> dict:
+        raise NotImplementedError()
 
     def __call__(self, params: dict):
         return self.model(params)
+
+
+def Star(bands: Optional[list]=None, backend: str="jax"):
+    if backend == "jax":
+        from ._jax.star import Star
+    else:
+        message = f"Backend value {backend!r} is invalid."
+        raise ValueError(message)
+    return Star(bands, backend=backend)
