dfa176281ef20288f8d475950f6b5dd43a065cad
Alex Lyttle <a.j.lyttle@outlook.com>
2024-01-25 17:02:33 +0000

    Add seismo

--- src/celestify/_jax/star.py
+++ src/celestify/_jax/star.py
@@ -21,13 +21,17 @@ class Star(StarBase):
         determs["log_age"] = log_age = outputs[0] - 9  # log(age) in Gyr
         determs["log_Teff"] = log_teff = outputs[1]
         determs["log_R"] = log_rad = outputs[2]
-        determs["log_Dnu"] = outputs[3]
+        determs["log_Dnu"] = log_dnu = outputs[3]
         determs["log_g"] = log_g = log_mass - 2 * log_rad + self.log_g_sun
         determs["log_L"] = log_lum = 2 * log_rad + 4 * (log_teff - self.log_teff_sun)
 
         determs["Teff"] = teff = 10**log_teff
         determs["R"] = 10**log_rad
         determs["age"] = 10**log_age
+
+        determs["Dnu"] = 10**log_dnu
+        determs["log_numax"] = self.log_numax_sun + log_g - self.log_g_sun - 0.5 * (log_teff - self.log_teff_sun)
+        determs["numax"] = 10**determs["log_numax"]
 
         if self.bolometric_corrections is None:
             return determs
@@ -41,6 +45,7 @@ class Star(StarBase):
         #     axis=-1
         # )
         bc = self.bolometric_corrections(inputs).squeeze()
+
         determs["bol_mag"] = bol_mag = self.bol_mag_sun - 2.5 * log_lum
         determs["abs_mag"] = abs_mag = bol_mag - bc
 
@@ -49,6 +54,8 @@ class Star(StarBase):
 
         # determs["distance"] = 1 / params["plx"]
         determs["mag"] = abs_mag - 5 * (1 - jnp.log10(params["distance"]))
-        determs["plx"] = 1 / params["distance"]
+        determs["plx"] = 1e3 / params["distance"]  # parallax in mas
 
+        determs["G"] = determs["mag"][2]
+        determs["BP_RP"] = bc[1] - bc[0]
         return determs
