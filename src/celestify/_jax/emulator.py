86c77cc3cb6c53200b4478b00bf6e7d2caf88203
Alex Lyttle <a.j.lyttle@outlook.com>
2024-03-04 22:29:51 +0000

    Remove need for tensorflow by using weights only

--- src/celestify/_jax/emulator.py
+++ src/celestify/_jax/emulator.py
@@ -9,9 +9,29 @@ class Emulator(EmulatorBase):
     def __init__(self):
         super(Emulator, self).__init__()
         self.weights = [jnp.array(w) for w in self.weights]
-        self.offset = jnp.array(self.offset)
-        self.scale = jnp.array(self.scale)
+        self.bias = [jnp.array(b) for b in self.bias]
+        self.input_loc = jnp.array(self.input_loc)
+        self.input_scale = jnp.array(self.input_scale)
+        self.output_loc = jnp.array(self.output_loc)
+        self.output_scale = jnp.array(self.output_scale)
 
+    # def model(self, x: ArrayLike) -> jnp.ndarray:
+    #     """Emulator model.
+
+    #     Args:
+    #         x (array-like): Neural network inputs.
+
+    #     Returns:
+    #         jax.numpy.ndarray: Neural network outputs.
+    #     """
+    #     x = jnp.array(x)
+    #     x -= self.weights[0]
+    #     x /= self.weights[1]**0.5
+    #     for w, b in zip(self.weights[3:-2:2], self.weights[4:-1:2]):
+    #         x = relu(jnp.matmul(x, w) + b)
+    #     x = jnp.matmul(x, self.weights[-2]) + self.weights[-1]
+    #     return self.offset + self.scale * x
+    
     def model(self, x: ArrayLike) -> jnp.ndarray:
         """Emulator model.
 
@@ -22,9 +42,9 @@ class Emulator(EmulatorBase):
             jax.numpy.ndarray: Neural network outputs.
         """
         x = jnp.array(x)
-        x -= self.weights[0]
-        x /= self.weights[1]**0.5
-        for w, b in zip(self.weights[3:-2:2], self.weights[4:-1:2]):
+        x -= self.input_loc
+        x /= self.input_scale
+        for w, b in zip(self.weights[:-1], self.bias[:-1]):
             x = relu(jnp.matmul(x, w) + b)
-        x = jnp.matmul(x, self.weights[-2]) + self.weights[-1]
-        return self.offset + self.scale * x
+        x = jnp.matmul(x, self.weights[-1]) + self.bias[-1]
+        return self.output_loc + self.output_scale * x
--- src/celestify/emulator.py
+++ src/celestify/emulator.py
@@ -1,19 +1,34 @@
-import os
-from tensorflow.keras.models import load_model
+import os, h5py
+# from tensorflow.keras.models import load_model
 
 from . import PACKAGEDIR
 
 
 class EmulatorBase:
-    PATH = os.path.join(PACKAGEDIR, "emulator")
+    # PATH = os.path.join(PACKAGEDIR, "emulator")
+    PATH = os.path.join(PACKAGEDIR, "data", "emulator_weights.h5")    
 
     def __init__(self):
         # Currently uses tensorflow to load weights
         # TODO: load only weights to remove tensorflow dependence
-        model = load_model(self.PATH)
-        self.weights = model.get_weights()
-        self.offset = model.layers[-1].offset
-        self.scale = model.layers[-1].scale
+        # model = load_model(self.PATH)
+        # self.weights = model.get_weights()
+        # self.offset = model.layers[-1].offset
+        # self.scale = model.layers[-1].scale
+
+        keys = [f"dense_{i}" for i in range(7, 14)]
+        weights = []
+        bias = []
+        with h5py.File(self.PATH) as file:
+            for key in keys:
+                weights.append(file[key][key]["kernel:0"][()])
+                bias.append(file[key][key]["bias:0"][()])
+            self.input_loc = file["normalization/mean:0"][()]
+            self.input_scale = file["normalization/variance:0"][()]**0.5
+            self.output_loc = file["rescaling/offset"][()]
+            self.output_scale = file["rescaling/scale"][()]
+        self.weights = weights
+        self.bias = bias
 
     def model(self, x):
         raise NotImplementedError()
