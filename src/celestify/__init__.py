261ac106bd5e0103a3095ebb7fc86addd383de9c
Alex Lyttle <a.j.lyttle@outlook.com>
2024-03-07 14:09:37 +0000

    Add version info

--- src/celestify/__init__.py
+++ src/celestify/__init__.py
@@ -1,3 +1,5 @@
 import os
+import importlib.metadata
 
+__version__ = importlib.metadata.version(__package__)  # only works if package installed via pip
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
